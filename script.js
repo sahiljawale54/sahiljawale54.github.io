c4477f83b5c31751980184a9d296a849ee88a137
sahiljawale54 <20510057.dypit@dypvp.edu.in>
2022-11-01 21:13:12 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll',function(){
+  var header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+window.onscroll = () => {
+  var header = document.querySelector('header');
+  if(this.scrollY <= 10) header.className = ''; else header.className = 'scroll';
+};
+
+function togglemenu(){
+  var menutoggle = document.querySelector('.toggle');
+  var menu = document.querySelector('.menu');
+  menutoggle.classList.toggle('active');
+  menu.classList.toggle('active');
+}
